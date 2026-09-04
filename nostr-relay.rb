7d95e7b1733ba4532be126d8c8f3fe06ba27c1c7
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-09-04 18:30:55 +0900

    Advertise NIP-78 support

--- nostr-relay.rb
+++ nostr-relay.rb
@@ -288,7 +288,7 @@ end
 RELAY_INFO = {
   "name" => "mruby-nostr-relay",
   "description" => "A Nostr relay written in mruby",
-  "supported_nips" => [1, 4, 9, 11, 26, 70],
+  "supported_nips" => [1, 4, 9, 11, 26, 70, 78],
   "relay_countries" => (ENV['RELAY_COUNTRIES'] || "JP").split(',').map(&:strip).reject(&:empty?),
   "software" => "mruby-nostr-relay",
   "version" => "0.1.0"
